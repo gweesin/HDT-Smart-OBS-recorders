cfa6e2d545faf886cd7ca58f5dfdcdf785e5b2f0
GweesinChan <gweesin2000@gmail.com>
2025-01-26 19:19:36 +0800

    chore: init

---
+++ README.md
@@ -0,0 +1,12 @@
+# Hearthstone game OBS recorder
+
+Hearthstone deck tracker plugin which automatically starts and stops OBS recordings when a hearthstone game starts and ends.
+
+To use this you will need:
+1. [OBS](https://obsproject.com/)
+2. [obs-websocket](https://obsproject.com/forum/resources/obs-websocket-remote-control-obs-studio-from-websockets.466/)
+3. [Hearthstone Deck Tracker](https://hsreplay.net/downloads/)
+
+# How to install
+1. Download the plugin DLL file from the [latest release](https://github.com/darksworm/hearthstone-obs-recorder/releases/latest)
+2. Follow the [HDT plugin installation guide](https://github.com/HearthSim/Hearthstone-Deck-Tracker/wiki/Available-Plugins)
---
+++ RecorderPlugin/Recorder.cs
@@ -0,0 +1,85 @@
+﻿using OBSWebsocketDotNet;
+using System;
+
+namespace RecorderPlugin
+{
+    internal class Recorder
+    {
+        public class ConnectionFailedException : Exception { }
+
+        public class AuthorizationFailedException : Exception { }
+
+        OBSWebsocket OBS = new OBSWebsocket();
+
+        private long NextStopTime = 0;
+
+        private string ConnectionString = String.Empty;
+        private string Password = String.Empty;
+
+        public void Connect()
+        {
+            try
+            {
+                OBS.Connect(ConnectionString, Password);
+            }
+            catch (AuthFailureException)
+            {
+                throw new AuthorizationFailedException();
+            }
+
+            if (!OBS.IsConnected)
+            {
+                throw new ConnectionFailedException();
+            }
+        }
+
+        public void UpdateSettings(string ip, string port, string password = "")
+        {
+            ConnectionString = $"ws://{ip}:{port}";
+            Password = password;
+        }
+
+        internal void StartRecording()
+        {
+            if (NextStopTime > 0)
+            {
+                NextStopTime = 0;                
+                StopRecording();
+            }
+
+            if (OBS.IsConnected)
+            {
+                OBS.StartRecord();
+            }
+        }
+
+        internal void StopAfter(long millis)
+        {
+            NextStopTime = Timestamp + millis;
+        }
+
+        internal void Unload()
+        {
+            OBS.Disconnect();
+        }
+
+        internal void Update()
+        {
+            if (NextStopTime > 0 && (Timestamp > NextStopTime || Hearthstone_Deck_Tracker.API.Core.Game.IsInMenu))
+            {
+                NextStopTime = 0;
+                StopRecording();
+            }
+        }
+
+        private long Timestamp => new DateTimeOffset(DateTime.UtcNow).ToUnixTimeSeconds();
+
+        private void StopRecording()
+        {
+            if (OBS.IsConnected && OBS.GetRecordStatus().IsRecording)
+            {
+                OBS.StopRecord();
+            }
+        }
+    }
+}
